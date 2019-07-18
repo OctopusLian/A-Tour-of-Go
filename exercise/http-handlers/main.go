87b44f0b55e75fe5fbea8e1a70d391decaaf6bea
OctopusLian <zoctopus@qq.com>
2019-07-18 21:59:05 +0800

    添加部分练习的说明

--- exercise/http-handlers/main.go
+++ exercise/http-handlers/main.go
@@ -5,7 +5,18 @@ import (
 	"net/http"
 )
 
+type String string
+
+type Struct struct {
+	Greeting string
+	Punct    string
+	Who      string
+}
+
 func main() {
 	// your http.Handle calls here
 	log.Fatal(http.ListenAndServe("localhost:4000", nil))
+
+	http.Handle("/string", String("I'm a frayed knot."))
+	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
 }
